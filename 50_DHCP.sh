3b30cac0ef00e4d165b7b7fe7dfafd7f57b4cb0c
John031265 <135910513+John031265@users.noreply.github.com>
2025-11-20 17:16:05 +0000

    Add files via upload

---
+++ 50_DHCP.sh
@@ -0,0 +1,4 @@
+sudo cp /etc/netplan/50-cloud-init.DHCP /etc/netplan/50-cloud-init.yaml
+sudo netplan try
+clear
+ip a
---
+++ 50_IP.sh
@@ -0,0 +1,4 @@
+sudo cp /etc/netplan/50-cloud-init.IP /etc/netplan/50-cloud-init.yaml
+sudo netplan try
+clear
+ip a
